acbd238d4d1c0c0c3b52859d0a87319ab1fbe1b8
SergeyBorozdin <borozinsm@yandex.ru>
2023-01-20 18:22:10 +0300

    added func is_set_bit, set_bit, inverse_bit, reset_bit, get_row, get_col

--- byte.c
+++ byte.c
@@ -1,40 +1,52 @@
 #include "byte.h"
 
-int STATE = 7;
+// int STATE = 7;
 
 /*
     отражает совпадение битов в двоичной записи
 */
-void func_byte(int(flag)) {
-  if (flag & STATE != 0) {
-    printf("1 - STATE\n");
-  }
-}
+// void func_byte(int(flag)) {
+//   if (flag & STATE != 0) {
+//     printf("1 - STATE\n");
+//   }
+// }
 
 /*
     проверка бита
 */
-bool is_set_bit(int number, int index) {}
+bool is_set_bit(int number, int index) { return (number & (1 << index)) != 0; }
 
 /*
     установка бита в 1
 */
-bool set_bit(int number, int index) {}
+bool set_bit(int number, int index) { return number | (1 << index); }
 
 /*
     инверсия бита
 */
-bool inverse_bit(int number, int index) {}
+bool inverse_bit(int number, int index) { return number ^ (1 << index); }
 
 /*
     установкака бита в 0
 */
-bool reset_bit(int number, int index) {}
+bool reset_bit(int number, int index) { return number & ~(1 << index); }
 
-int main() {
-  int STATE_1 = 1;
-  int STATE_2 = 2;
-  int STATE_3 = 4;
-  func_byte(STATE_1 | STATE_2 | STATE_3);
-  return 0;
-}
+/*
+    возвращает номер строки в битовом векторе
+*/
+int get_row(int bit) {
+  return (int)Math.floor(bit >> 4);
+}  // Math.floor почитай это что, 24 минута
+
+/*
+    возвращает номер столбца в битовом векторе
+*/
+int get_col(int bit) { return bit % 16; }
+
+// int main() {
+//   int STATE_1 = 1;
+//   int STATE_2 = 2;
+//   int STATE_3 = 4;
+//   func_byte(STATE_1 | STATE_2 | STATE_3);
+//   return 0;
+// }
--- byte.h
+++ byte.h
@@ -1,9 +1,16 @@
 #ifndef _BYTE_H_
 #define _BYTE_H_
 
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// 250 интов  это 205 * 32 бита = 8000 бит
+int bit_vector[250];
+
+int get_row();
+int get_col();
 
 bool is_set_bit(int number, int index);
 bool set_bit(int number, int index);
